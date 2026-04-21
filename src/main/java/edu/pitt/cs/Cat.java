62582e734e081da1db77fcfa19d3d3f59a4d74b9
Sam Gombiner <116911252+sgombiner@users.noreply.github.com>
2026-04-20 21:12:32 -0400

    push this

--- src/main/java/edu/pitt/cs/Cat.java
+++ src/main/java/edu/pitt/cs/Cat.java
@@ -1,7 +1,7 @@
 package edu.pitt.cs;
 
 import org.mockito.Mockito;
-import static org.mockito.Mockito.*; 
+import static org.mockito.Mockito.*;
 
 public interface Cat {
 	public static Cat createInstance(InstanceType type, int id, String name) {
@@ -13,17 +13,18 @@ public interface Cat {
 			case SOLUTION:
 				return new CatSolution(id, name);
 			case MOCK:
-			    // TODO: Return a mock object that emulates the behavior of a real object.
+				// TODO: Return a mock object that emulates the behavior of a real object.
+				// test
 				return null;
 			default:
-				assert(false);
+				assert (false);
 				return null;
 		}
 	}
 
 	// WARNING: You are not allowed to change any part of the interface.
 	// That means you cannot add any method nor modify any of these methods.
-	
+
 	public void rentCat();
 
 	public void returnCat();
